824fa6c63d2918b05781f2154bb660280d4477df
sovermind <sovermind11@gmail.com>
2021-01-25 22:59:22 -0800

    Start skill scriptable object implementation

---
+++ Assets/Scripts/ScriptableObjects/ActiveSkillSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Skill/ActiveSkill")]
+public class ActiveSkillSO : SkillSO {
+	public int attackRangeRadInCell;
+	public int effectiveRangeRadInCell;
+}
---
+++ Assets/Scripts/ScriptableObjects/SkillSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillSO : ScriptableObject {
+	public string skillName;
+	public Sprite skillIcon;
+	public int level;
+	public int maxLevel;
+	public int[] costToUpgrade;
+}
