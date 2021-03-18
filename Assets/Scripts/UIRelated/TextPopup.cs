9062914ba64cac776661c944e0d8c7860d271c90
sovermind <sovermind11@gmail.com>
2021-03-17 23:05:51 -0700

    Create a text pop up
    This can be used to show a damage amount the characters takes

---
+++ Assets/Scripts/UIRelated/TextPopup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPopup : MonoBehaviour {
+
+	/// <summary>
+	/// Create a text pop up at desired position with a given text wants to show up
+	/// The pop up will disappear after certain time
+	/// </summary>
+	public static void Create(Vector3 pos, string popupText) {
+		//https://www.youtube.com/watch?v=iD1_JczQcFY
+	}
+
+	void Update() {
+        
+    }
+}
